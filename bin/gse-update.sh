d6e91f4f280115322732d63f7d247e7a2f7d9512
Julian Pawlowski <julian.pawlowski@gmail.com>
2013-01-26 19:55:21 +0100

    initial commit for gse-update.sh

---
+++ bin/gse-update.sh
@@ -0,0 +1,229 @@
+#!/bin/bash
+#
+# Gemeinschaft 5
+# System Environment update script
+#
+# Copyright (c) 2013, Julian Pawlowski <jp@jps-networks.eu>
+# See LICENSE.GBE file for details.
+#
+
+# Enforce root rights
+#
+if [[ ${EUID} -ne 0 ]];
+	then
+	echo "ERROR: `basename $0` needs to be run as root. Aborting ..."
+	exit 1
+fi
+
+# General settings
+[ -f /etc/gemeinschaft/system.conf ] && source /etc/gemeinschaft/system.conf || echo "FATAL ERROR: Local configuration file in /etc/gemeinschaft/system.conf missing"
+[[ x"${GSE_DIR}" == x"" ]] && exit 1
+GSE_UPDATE_DIR="${GSE_DIR}.update"
+
+# check each command return codes for errors
+#
+set -e
+
+# Run switcher
+#
+case "$1" in
+	--help|-h|help)
+	echo "Usage: $0 [ --factory-reset ]"
+	exit 0
+	;;
+
+	--factory-reset)
+	MODE="factory-reset"
+	while true; do
+    	echo "ATTENTION! This will do a factory reset of the SYSTEM ENVIRONMENT, all customizations will be LOST!"
+    	read -p "Continue? (y/n) : " yn
+
+    	case $yn in
+        	Y|y )
+
+				break
+			;;
+
+        	* )
+				echo "Aborting ...";
+				break
+			;;
+    	esac
+
+		exit
+	done
+	;;
+
+	--force-update)
+	MODE="update"
+	;;
+
+	--force-init)
+	MODE="init"
+	;;
+
+	*)
+	MODE="update"
+
+	# Check for live status
+	#
+	if [[ x`cat /proc/cmdline | grep boot=live` != x"" ]]
+		then
+		echo "LIVE mode detected. Aborting ..."
+		exit 1
+	fi
+
+	clear
+	echo "
+***    ------------------------------------------------------------------
+***     GEMEINSCHAFT SYSTEM ENVIRONMENT UPDATE
+***     Current version: ${GSE_VERSION}
+***     Branch: ${GSE_BRANCH}
+***     Base System Build: ${GS_BUILDNAME}
+***    ------------------------------------------------------------------
+***
+***     ATTENTION! Please read the following information CAREFULLY!
+***     ===========================================================
+***
+***     This script will prepare your system to upgrade to the latest
+***     system environment scripts source code.
+***     Updating the system environment via this script is NOT supported,
+***     we recommend to use the backup/restore function via the web
+***     interface instead. This will ensure also other parts of the
+***     system environment are up-to-date.
+***
+"
+
+	while true; do
+	    read -p "Do you want to continue? (Y/n) : " yn
+	    case $yn in
+	        y|Y ) break;;
+	        * ) echo "Aborting ..."; exit;;
+	    esac
+	done
+
+	;;
+esac
+
+# Prepare for system update
+#
+if [[ "${MODE}" == "update" ]]; then
+
+	echo -e "Preparing update of Gemeinschaft System Environment ...\n"
+
+	# Remove any old update files
+	[[ -d "${GSE_UPDATE_DIR}" ]] && rm -rf "${GSE_UPDATE_DIR}"
+	[[ -d "${GSE_UPDATE_DIR}.tmp" ]] && rm -rf "${GSE_UPDATE_DIR}.tmp"
+	
+	# Make a copy of current files
+	cp -r "${GSE_DIR}" "${GSE_UPDATE_DIR}.tmp"
+	cd "${GSE_UPDATE_DIR}.tmp"
+
+	# Add Git remote data to pull from it
+	git clean -fdx && git reset --hard HEAD
+	git remote add -t "${GSE_BRANCH}" origin "${GSE_GIT_URL}"
+
+	# Setup Github user credentials for login
+	#
+	if [ ! -z "${GSE_GIT_USER}" -a ! -z "${GSE_GIT_PASSWORD}" ]
+		then
+		echo "Github credentials found!"
+echo "machine Github.com
+login ${GSE_GIT_USER}
+password ${GSE_GIT_PASSWORD}
+" >  ~/.netrc
+	fi
+
+	set +e
+	c=1
+	while [[ $c -le 5 ]]
+	do
+		git remote update 2>&1
+		if [ "$?" = "0" ]
+			then
+			break;
+		else
+			[[ $c -eq 5 ]] && exit 1
+			(( c++ ))
+			echo "$c. try in 3 seconds ..."
+			sleep 3
+		fi
+	done
+
+	c=1
+	while [[ $c -le 5 ]]
+	do
+		git pull origin "${GSE_BRANCH}" 2>&1
+		if [ "$?" -eq "0" ]
+			then
+			break;
+		else
+			[[ $c -eq 5 ]] && exit 1
+			(( c++ ))
+			echo "$c. try in 3 seconds ..."
+			sleep 3
+		fi
+	done
+	set -e
+
+	rm -rf ~/.netrc
+
+	# Make sure we checkout the latest tagged version in case we are in the master branch, otherwise set HEAD to the latest revision of GSE_BRANCH
+	[ "${GSE_BRANCH}" == "master" ] && git checkout "`git tag -l | tail -n1`" || git checkout "${GSE_BRANCH}"
+
+	# Check version compatibility, allow auto-update only for minor versions
+	GSE_GIT_VERSION="`git tag --contains HEAD`"
+	GSE_REVISION="`git --git-dir="${GSE_DIR}/.git" rev-parse HEAD`"
+	GSE_GIT_REVISION="`git rev-parse HEAD`"
+	if [[ "${GSE_GIT_REVISION}" == "${GSE_REVISION}" ]]; then
+		rm -rf "${GSE_UPDATE_DIR}"*
+		echo -e "\n\n***    ------------------------------------------------------------------"
+		echo -e "***     You have already installed the latest version, no update needed."
+		echo -e "***    ------------------------------------------------------------------\n\n"
+		exit 0
+	elif [[ "${GSE_GIT_VERSION:0:3}" == "${GSE_VERSION:0:3}" || x"${GSE_GIT_VERSION}" == x"" ]]; then
+		[ "${GSE_BRANCH}" != "master" ] && GSE_GIT_VERSION="from ${GSE_BRANCH} branch"
+		mv "${GSE_UPDATE_DIR}.tmp" "${GSE_UPDATE_DIR}"
+		echo -e "\n\n***    ------------------------------------------------------------------"
+		echo -e "***     Scheduled update to new version ${GSE_GIT_VERSION}.\n***     Please reboot the system to start the update process."
+		echo -e "***    ------------------------------------------------------------------\n\n"
+	else
+		rm -rf "${GSE_UPDATE_DIR}"*
+		echo -e "\n\n***    ------------------------------------------------------------------"
+		echo -e "***     Update to the next major version ${GSE_GIT_VERSION} is not supported\n***     via this script.\n***     Please use backup & restore via web interface."
+		echo -e "***    ------------------------------------------------------------------\n\n"
+		exit 1
+	fi
+
+	#TODO uninstall old version first to make sure old system files can be removed permanently
+	#
+
+	echo "** Rename and backup old files in \"${GSE_DIR}\""
+	[ ! -d "${GSE_DIR}.${GSE_VERSION}" ] && mv "${GSE_DIR}" "${GSE_DIR}.${GSE_VERSION} "|| rm -rf "${GSE_DIR}"
+	mv ${GSE_UPDATE_DIR} ${GSE_DIR}
+fi
+
+
+# Run essential init and update commands
+#
+if [[ "${MODE}" == "init" || "${MODE}" == "update" ]]; then
+	# Remove Git remote reference
+	#
+	echo "** Remove Git remote reference"
+	GS_GIT_REMOTE="`git --git-dir="${GSE_DIR_NORMALIZED}/.git" remote`"
+	for _REMOTE in ${GSE_GIT_REMOTE}; do
+		cd "${GSE_DIR_NORMALIZED}"; git remote rm ${_REMOTE}
+	done
+
+	# Enforce debug level according to GSE_ENV
+	#
+	/usr/local/bin/gs-change-state.sh
+fi
+
+
+# Finalize update
+#
+if [[ "${MODE}" == "update" ]]; then
+	echo "** Enforcing file permissions and security settings ..."
+	/usr/local/bin/gs-enforce-security.sh | grep -Ev retained | grep -Ev "no changes" | grep -Ev "nor referent has been changed"
+fi
